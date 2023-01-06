ff983fc3aa347c248e3af19946e78f420fc76a24
mateus <mateusinfo36@gmail.com>
2023-01-06 13:31:33 -0300

    Polimorfismo e criação do editor de video

---
+++ src/EditorVideo.java
@@ -0,0 +1,8 @@
+public class EditorVideo extends Funcionario{//Gerente é um funcionrio esta herdando a class funcionario
+    public double  getBonificacao(){
+        return super.getBonificacao() + 100;
+
+    }
+
+
+}
--- src/TesteReferencias.java
+++ src/TesteReferencias.java
@@ -1,2 +1,21 @@
 public class TesteReferencias {
+    public static void main(String[] args) {
+        Gerente g1 = new Gerente();
+
+        g1.setNome("Mateus");
+        g1.setSalario(2000);
+
+
+        Funcionario f = new Funcionario();
+        f.setSalario(1000);
+
+        EditorVideo ev = new EditorVideo();
+        ev.setSalario(2500);
+        ControleBonificacao controle = new ControleBonificacao();
+        controle.registra(g1);
+        controle.registra(f);
+        controle.registra(ev);
+
+        System.out.println(controle.getSoma());
+    }
 }
